18938ed737088a63baf6a17a395f745f6096a936
Daniel Roth <daniel@encodo.ch>
2017-09-15 12:07:27 +0200

    Fix routing in production

--- src/store/configureStore.prod.ts
+++ src/store/configureStore.prod.ts
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { routerMiddleware } from 'react-router-redux';
 
+import { history } from '../history';
 import { rootReducer } from '../reducers';
 
 const configureStore = (preloadedState: Partial<TState>) => createStore(
   rootReducer,
   preloadedState,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, routerMiddleware(history))
 );
 
 export default configureStore;
