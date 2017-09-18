0b0bcc9e4a95b99ce8683162253c0afbb6a66546
Daniel Roth <daniel@encodo.ch>
2017-09-18 10:37:35 +0200

    Add distance/elevation to the report

--- src/components/YearReport/Report.tsx
+++ src/components/YearReport/Report.tsx
@@ -5,22 +5,25 @@ import * as fromReducers from '../../reducers';
 
 import { LookupView } from '../People';
 
+type TEnhancedTour = TTour & { routeObj: TRoute | undefined };
 type TStateProps = {
-  tours: TTour[];
+  tours: TEnhancedTour[];
 };
 type TDispatchProps = {};
 type TOwnProps = {};
 type TProps = TStateProps & TDispatchProps & TOwnProps;
 
 const mapStateToProps = (state: TState, ownProps: TOwnProps): TStateProps => ({
-  tours: fromReducers.getToursForYear(state, 2017)
+  tours: fromReducers
+    .getToursForYear(state, 2017)
+    .map(t => ({ ...t, routeObj: fromReducers.getRoute(state, t.route) }))
 });
 
 export const Report = connect(mapStateToProps)((props: TProps) => {
-  const toursByPerson = props.tours.reduce<{ [participant: string]: TTour[] }>(
+  const toursByPerson = props.tours.reduce<{ [participant: string]: TEnhancedTour[] }>(
     (prev, cur) => ({
       ...prev,
-      ...cur.participants.reduce<{ [participant: string]: TTour[] }>(
+      ...cur.participants.reduce<{ [participant: string]: TEnhancedTour[] }>(
         (partPrev, curPart) => ({
           ...(partPrev || {}),
           [curPart]: [...((partPrev || {})[curPart] || []), cur]
@@ -32,13 +35,22 @@ export const Report = connect(mapStateToProps)((props: TProps) => {
 
   const toursByPersonSummedUp = Object
     .keys(toursByPerson)
-    .reduce<{ participant: string; totalPoints: number; tourCount: number; tours: TTour[] }[]>(
+    .reduce<{
+      participant: string;
+      totalPoints: number;
+      distance: number;
+      elevation: number;
+      tourCount: number;
+      tours: TEnhancedTour[]
+    }[]>(
     (prev, p) => ([
       ...prev,
       {
         participant: p,
         tours: toursByPerson[p],
         tourCount: toursByPerson[p].length,
+        elevation: toursByPerson[p].reduce((sum, cur) => sum + (cur.routeObj ? cur.routeObj.elevation : 0), 0),
+        distance: toursByPerson[p].reduce((sum, cur) => sum + (cur.routeObj ? cur.routeObj.distance : 0), 0),
         totalPoints: toursByPerson[p].reduce((sum, cur) => sum + cur.points, 0)
       }
     ]),
@@ -52,9 +64,13 @@ export const Report = connect(mapStateToProps)((props: TProps) => {
     <div>
       <p>Total Points {props.tours.length}</p>
       {toursByPersonSummedUp.map(p => {
-        const tours = p;
+        const tours = p.tours;
         return (
-          <p key={p.participant}><LookupView values={[p.participant]} /> {tours.totalPoints} / {tours.tourCount}</p>
+          <p key={p.participant}>
+            <LookupView values={[p.participant]} /> {p.totalPoints} / {p.tourCount}
+            ({p.distance}/{p.elevation})
+            ({tours.map(t => t.routeObj ? t.routeObj.name : t.route).join()})
+          </p>
         );
       })}
       <pre>{JSON.stringify(props.tours, null, 2)}</pre>
